f0a1cf267f05ad66156090a71d322125d00706de
Abdullahi-El <91474775+Abdullahi-El@users.noreply.github.com>
2023-01-18 17:21:41 +0100

    Add files via upload

---
+++ Test1/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import"./App.css";
+import Functionalcomp from "/components/functionalcomponents"
+import Classcomp  from "./Components/classComp";
+function App() {
+   return (
+   <div>
+   <h1>Hello wellcome to my first react </h1>
+   
+   <h1>this video will be there to us</h1>
+</div>
+   );
+}
+
+export default App;
---
+++ Test1/Components/classComp.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Classcomp extends React.Component  {
+render(){
+return <p>this is the class component </p>
+
+
+}
+
+}
+export default  Classcomp;
